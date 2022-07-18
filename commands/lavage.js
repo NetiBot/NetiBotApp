588e310d0cc26a1a1a13c543a197f573c85aace0
Kevin Roney <ktroney.pdx@gmail.com>
2022-07-18 16:55:44 -0700

    added lavage to delete N amount of messages from the chat

---
+++ commands/lavage.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('lavage')
+    .setDescription('Lavage an amount of messages down the drain(we hope, you savage)') 
+    .addIntegerOption((option) => { 
+      return option
+        .setName('amount') 
+        .setDescription('Amount of message to delete') 
+        .setRequired(true); 
+    }),
+  async execute(interaction) {
+    if(!interaction.member.permissions.has('MANAGE_MESSAGES')) return interaction.reply({ content: 'You don\'t have `MANAGE_MESSAGES` permission to use this command!' }); 
+    if(!interaction.guild.me.permissions.has('MANAGE_MESSAGES')) return interaction.reply({ content: 'I don\'t have `MANAGE_MESSAGES` permission to execute this command!' }); 
+
+    const amount = interaction.options.getInteger('amount'); 
+
+    if(isNaN(amount) || amount < 1) { 
+      return interaction.reply({ content: '**Please specify a valid amount between 1 - 100!**', ephemeral: true });
+    }
+
+    if(parseInt(amount) > 99) { 
+      return interaction.reply({ content: '**I can only delete 99 messages once!', ephemeral: true });
+    } else {
+      try{
+        const { size } = await interaction.channel.bulkDelete(amount); 
+        await interaction.reply({ content: `Deleted ${size} messages.`, ephemeral: true });
+      } catch(e) { 
+        console.log(e);
+        interaction.reply({ content: 'I cannot delete messages that is older than 14 days.', ephemeral: true });
+      }
+    }
+  }
+};
