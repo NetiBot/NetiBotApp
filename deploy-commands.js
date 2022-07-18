1fd0bd58a5d3064f91770eb8a3612e742eb5302f
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-18 12:05:16 -0700

    in deploy commands.js made commands global vs guild based

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,12 +22,8 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 rest
-  .put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
-    { body: commands }
-  )
+  .put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: commands,
+  })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
