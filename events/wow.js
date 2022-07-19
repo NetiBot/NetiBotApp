b36dd9c771c5c5f17f39b663d6d6644c5d97b8ce
JeffAllison <itsjallison@gmail.com>
2022-07-19 10:06:17 -0700

    moved code to keep wow count from duplicating

--- events/wow.js
+++ events/wow.js
@@ -9,19 +9,23 @@ module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (message.content.includes('wow')) {
-      db.add('times.wow', 1);
-      const timesUsed = await db.get('times.wow');
       if (message.author.bot) {
         return;
       }
+      db.add('times.wow', 1);
+      const timesUsed = await db.get('times.wow');
+
       const memeResult = await fetch(
         'https://owen-wilson-wow-api.herokuapp.com/wows/random'
       );
       const memeFile = await memeResult.json();
       const file = memeFile[0].video['1080p'];
       const attachment = new MessageAttachment(file);
-      await message.reply({ content: `You said wow,\nNetiBot has seen wow ${timesUsed} times since July 14th, 2022
-      `, files: [attachment] });
+      await message.reply({
+        content: `You said wow,\nNetiBot has seen wow ${timesUsed} times since July 14th, 2022
+      `,
+        files: [attachment],
+      });
     }
   },
 };
