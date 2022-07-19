a3e42086d72cce18519fd06a859aed2972ad2cac
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-19 14:29:14 -0700

    test for retreiving a random diagnosis and treatment passing

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,24 +1,24 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
+// const pool = require('../lib/utils/pool');
+// const setup = require('../data/setup');
 const request = require('supertest');
-const app = require('../server');
-// const Webmd = require('../lib/models/Webmd');
-
-jest.mock('../lib/controllers/webmds');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    // return setup(pool);
   });
   it('GET /webmds retrieves a random diagnosis and treatment', async () => {
     const res = await request(app).get('/webmds');
     expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      diagnosis: expect.any(String),
-      treatment: expect.any(String),
-    });
+    expect(res.body).toEqual([
+      {
+        _id: expect.any(String),
+        diagnosis: expect.any(String),
+        treatment: expect.any(String),
+      },
+    ]);
   });
   afterAll(async () => {
-    await pool.end();
+    // await pool.end();
   });
 });
