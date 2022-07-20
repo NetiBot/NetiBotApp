e960bfb83b8ac00ae70a60126ac28b451350e0d6
Cody-Wise <cody@sharptackmedia.com>
2022-07-20 10:28:37 -0700

    Updated birthday event to be case insensitive

--- events/birthday.js
+++ events/birthday.js
@@ -1,12 +1,14 @@
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
-    if (message.content.includes('birthday')) {
+    const bday = message.content.toLowerCase();
+    if (bday.includes('birthday')) {
       if (message.author.bot) {
         return;
       }
       const file = 'https://youtu.be/vdVnnMOTe3Q';
-      const birthday = 'https://c.tenor.com/LSgWvTFLZ64AAAAC/birthday-cake-its-your-birthday.gif';
+      const birthday =
+        'https://c.tenor.com/LSgWvTFLZ64AAAAC/birthday-cake-its-your-birthday.gif';
       console.log(file);
       await message.reply(`${birthday}\n ${file}`);
     }
