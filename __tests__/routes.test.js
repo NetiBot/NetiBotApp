33b00597a1f6c81bfd06442274745c54b4729fc0
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-20 14:42:55 -0700

    passing test for update webmds

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -11,9 +11,12 @@ describe('backend-express-template routes', () => {
     expect(res.status).toBe(200);
     expect(res.body).toEqual([
       {
+        __v: 0,
+        createdAt: expect.any(String),
         _id: expect.any(String),
         diagnosis: expect.any(String),
         treatment: expect.any(String),
+        updatedAt: expect.any(String),
       },
     ]);
   });
@@ -25,7 +28,18 @@ describe('backend-express-template routes', () => {
     expect(res.body.diagnosis).toBe('Test');
     expect(res.body.treatment).toBe('Test Again');
   });
+<<<<<<< Updated upstream
   afterAll(async () => {
     // await pool.end();
+=======
+  it('UPDATE /webmds/:id updates a diagnosis and treatment', async () => {
+    const res = await request(app)
+      .put('/webmds/62d87221e08c9882c1d2c2f3')
+      .send({ diagnosis: 'Test', treatment: 'Test Again' });
+    console.log('res', res);
+    expect(res.status).toBe(200);
+    expect(res.body.diagnosis).toBe('Test');
+    expect(res.body.treatment).toBe('Test Again');
+>>>>>>> Stashed changes
   });
 });
